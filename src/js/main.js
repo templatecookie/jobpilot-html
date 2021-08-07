108503fa1b79cfe8de6e8217eb9a4d4bbd3b08d2
Mahedi Amin <mahedi_cse@hotmail.com>
2021-08-07 07:46:22 +0600

    file init

---
+++ src/js/main.js
@@ -0,0 +1,44 @@
+/*
+* ----------------------------------------------------------------------------------------
+    Template Name: mr stater
+    Template URI: https://spellbit.com/
+    Description: 
+    Author: mahedi amin
+    Author URI: https://mahediamin.com
+    Version: 1.0.0
+
+
+
+    -------------INDEX----------------
+   01.Mobile Menu
+   02.Sticky Menu
+   03.All Owl Slider
+   04.Offcanvas
+   05. Counter
+   06. scroll up
+   07. Magnify active
+   08. Preloder Active
+   09. WOW Active
+   10. PROGRESSBAR ACTIVATION
+   11. Tooltip activation
+   12. ui activation
+   13. Filter Acitve
+   14. Select Two Activation
+   15. Paralx Init
+
+* ----------------------------------------------------------------------------------------
+*/
+(function($) {
+
+
+    console.log("happy coding");
+ 
+ 
+ 
+ 
+ })(jQuery)
+ 
+ 
+ 
+ 
+ 

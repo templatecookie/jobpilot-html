5de8ef43f6b462ebdae27de1925212783f8b1b2f
Mahedi Amin <mahedi_cse@hotmail.com>
2021-08-19 11:39:22 +0600

    filter menu done
    working on slider range

--- src/js/main.js
+++ src/js/main.js
@@ -31,6 +31,22 @@
     });
   }
 
+  $(".has-children > .jobwidget_tiitle").on("click", function () {
+    var element = $(this).parent("li");
+    if (element.hasClass("open")) {
+      element.removeClass("open");
+      element.find("li").removeClass("open");
+      element.find("ul").slideUp(200);
+    } else {
+      element.addClass("open");
+      element.children("ul").slideDown(200);
+      element.siblings("li").children("ul").slideUp();
+      element.siblings("li").removeClass("open");
+      element.siblings("li").find("li").removeClass("open");
+      element.siblings("li").find("ul").slideUp();
+    }
+  });
+
   function stickyHeader() {
     let mainheader = $(".rt-sticky"),
       height = mainheader.outerHeight(),

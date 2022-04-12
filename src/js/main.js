cefb0162e3305141902765063ca8d7ea5ccff8b3
MD.JIHAD HOSSAIN <43439618+mdjihad560@users.noreply.github.com>
2022-04-12 15:42:41 +0600

    create: lagnguage selector

--- src/js/main.js
+++ src/js/main.js
@@ -600,48 +600,49 @@ $(window).Scrollax();
   }
 
    // Range Slider
-const sliderRange = document.querySelector('.sliderrange');
-const output = document.querySelector('#value-range');
-if (output) {
-  output.innerHTML = `${sliderRange.value} miles`;
-}
+// const sliderRange = document.querySelector('.sliderrange');
+// const output = document.querySelector('#value-range');
+// if (output) {
+//   output.innerHTML = `${sliderRange.value} miles`;
+// }
 
-if (sliderRange) {
-  sliderRange.oninput = function () {
-    output.innerHTML = `${this.value} miles`;
-  };
+// if (sliderRange) {
+//   sliderRange.oninput = function () {
+//     output.innerHTML = `${this.value} miles`;
+//   };
 
-  sliderRange.addEventListener('mousemove', function () {
-    var x = sliderRange.value;
+//   sliderRange.addEventListener('mousemove', function () {
+//     var x = sliderRange.value;
 
-    var color = `linear-gradient(
-      90deg,
-      #0066FF ${x}%,
-      rgb(218, 221, 229) ${x}%
-    )`;
+//     var color = `linear-gradient(
+//       90deg,
+//       #0066FF ${x}%,
+//       rgb(218, 221, 229) ${x}%
+//     )`;
 
-    sliderRange.style.background = color;
-  });
-}
+//     sliderRange.style.background = color;
+//   });
+// }
 
-if (range) {
-  noUiSlider.create(range, {
-    start: [20, 80],
-    connect: true,
-    range: {
-      min: [0],
-      max: [500],
-    },
-    format: wNumb({
-      decimals: 3,
-      thousand: '.',
-      suffix: ' ($)',
-    }),
-    tooltips: true,
-  });
-}
+// if (range) {
+//   noUiSlider.create(range, {
+//     start: [20, 80],
+//     connect: true,
+//     range: {
+//       min: [0],
+//       max: [500],
+//     },
+//     format: wNumb({
+//       decimals: 3,
+//       thousand: '.',
+//       suffix: ' ($)',
+//     }),
+//     tooltips: true,
+//   });
+// }
  
   
+$('#basic').flagStrap();
 
 
 })(jQuery);
